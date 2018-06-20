1dcf5b34f2453291417e2bc9b58b6597cc805540
Brian Faust <hello@brianfaust.me>
2018-06-20 18:08:02 +0300

    refactor: adjust v2 peer model fields

--- lib/ark_client/api/two/models/peer.ex
+++ lib/ark_client/api/two/models/peer.ex
@@ -1,23 +1,41 @@
-defmodule ArkClient.API.Two.Models.Peer do
+defmodule ArkClient.API.One.Models.Peer do
   @moduledoc """
   Peer model.
   """
   @derive Jason.Encoder
 
   defstruct [
-    :dummy
+    :ip,
+    :port,
+    :version,
+    :height,
+    :status,
+    :os,
+    :latency
   ]
 
   @type t() :: %__MODULE__{
-    dummy: Integer.t()
+    ip: String.t(),
+    port: Integer.t(),
+    version: String.t(),
+    height: Integer.t(),
+    status: String.t(),
+    os: String.t(),
+    latency: Integer.t()
   }
 
   @doc """
-  Builds an ArkClient.API.Two.Models.Peer struct from the API response map.
+  Builds an ArkClient.API.One.Models.Peer struct from the API response map.
   """
   def build(attributes) do
     %__MODULE__{
-      dummy: Map.get(attributes, "dummy")
+      ip: Map.get(attributes, "ip"),
+      port: Map.get(attributes, "port"),
+      version: Map.get(attributes, "version"),
+      height: Map.get(attributes, "height"),
+      status: Map.get(attributes, "status"),
+      os: Map.get(attributes, "os"),
+      latency: Map.get(attributes, "latency")
     }
   end
 end
