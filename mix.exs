163fe7f71ccd250d72e49f053bb447c311466190
Brian Faust <hello@brianfaust.me>
2018-07-17 12:39:57 +0300

    fix: add missing test_coverage to project section

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ArkEcosystem.Client.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
     ]
   end
 
