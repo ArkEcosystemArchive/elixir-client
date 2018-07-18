ffa404b0cdc205413c054c9c8160f3c8efee7875
Brian Faust <envoyer@pm.me>
2018-07-18 07:42:28 +0300

    chore: add missing maintainer and deps
    Signed-off-by: Brian Faust <envoyer@pm.me>

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule ArkEcosystem.Client.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:excoveralls, "~> 0.9.1", only: :test},
       {:hackney, "~> 1.10"},
       {:jason, "~> 1.0"},
@@ -41,7 +42,8 @@ defmodule ArkEcosystem.Client.MixProject do
 
   defp package() do
     [
-      files: ["config", "lib", "priv", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Brian Faust", "ArkEcosystem"],
+      files: ["config", "lib", "mix.exs", "README*", "LICENSE*"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/ArkEcosystem/elixir-client"}
     ]
