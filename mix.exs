bcf7435c304efea0b8e92e62628ae2f1ff2db70b
Brian Faust <hello@basecode.sh>
2018-12-24 05:22:52 +0200

    chore: bump version and update changelog

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ArkEcosystem.Client.MixProject do
   def project do
     [
       app: :arkecosystem_client,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: description(),
