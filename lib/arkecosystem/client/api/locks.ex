f58c2c9607cd85b9e75f3b09b05fa38f55601977
Edgar Goetzendorff <hello@dated.fun>
2019-10-19 01:40:05 +0200

    feat: add locks endpoints

---
+++ lib/arkecosystem/client/api/locks.ex
@@ -0,0 +1,27 @@
+defmodule ArkEcosystem.Client.API.Locks do
+  @moduledoc """
+  Documentation for ArkEcosystem.Client.API.Locks
+  """
+
+  import ArkEcosystem.Client
+
+  @spec list(Tesla.Client.t(), Keyword.t()) :: ArkEcosystem.Client.response()
+  def list(client, parameters \\ []) do
+    client |> get("locks", parameters)
+  end
+
+  @spec show(Tesla.Client.t(), String.t()) :: ArkEcosystem.Client.response()
+  def show(client, id) do
+    client |> get("locks/#{id}")
+  end
+
+  @spec search(Tesla.Client.t(), Keyword.t()) :: ArkEcosystem.Client.response()
+  def search(client, parameters) do
+    client |> post("locks/search", parameters)
+  end
+
+  @spec search(Tesla.Client.t(), Keyword.t()) :: ArkEcosystem.Client.response()
+  def search(client, parameters) do
+    client |> post("unlocked/search", parameters)
+  end
+end
